1156fcbf1cd0ae9f09db53e970e0ac1fe8c3ded3
jharnakhatun2 <jharnakhatun203@gmail.com>
2024-01-04 23:57:33 +0600

    update main branch

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-4xl'>Welcome to FastShopMart Staging Branch</h1>
+      <h1 className='text-4xl'>FastShopMart Main Branch</h1>
     </main>
   )
 }
