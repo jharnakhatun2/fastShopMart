a61ee05675f240bfd9fa8ee7a5644fc490800617
jharnakhatun2 <jharnakhatun203@gmail.com>
2024-01-05 11:55:38 +0600

    added login

---
+++ app/(common)/registration/page.js
@@ -0,0 +1,7 @@
+export default function Registration() {
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='text-4xl'>Registration</h1>
+    </main>
+    )
+}
