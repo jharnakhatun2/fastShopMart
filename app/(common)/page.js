0c10f60ca27ea4e9d189918bb94fb60af15e984e
jharnakhatun2 <jharnakhatun203@gmail.com>
2024-01-10 20:38:37 +0600

    added update

--- app/(common)/page.js
+++ app/(common)/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-4xl'>FastShopMart Jharna Branch</h1>
+      <h1 className='text-4xl'>Main Branch</h1>
     </main>
   )
 }
